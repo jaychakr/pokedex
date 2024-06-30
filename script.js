4c80f59f104472d80aede021154cb119ac7e840f
jaychakr <158199926+jaychakr@users.noreply.github.com>
2024-06-30 12:12:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ const getPokemon = async () => {
     spriteContainer.innerHTML = `
       <img id="sprite" src="${data.sprites.front_default}" alt="${data.name} front default sprite">
     `;
+    topContainer.style.backgroundColor = '#f0f1f7';
 
     // Set stats
     hp.textContent = data.stats[0].base_stat;
@@ -66,6 +67,7 @@ const resetDisplay = () => {
   specialAttack.textContent = '';
   specialDefense.textContent = '';
   speed.textContent = '';
+  topContainer.style.backgroundColor = '#232323';
 };
 
 searchForm.addEventListener('submit', e => {
